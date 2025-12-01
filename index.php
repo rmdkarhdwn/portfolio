86b7e2fe2b0c8b85759dd9291d79b4fc091c5cd3
rmdkarhdwn <dkgkrlrnlvksgsk@gmail.com>
2025-12-01 23:41:18 +0900

    feat : 글자추가, 디자인 생성

--- index.php
+++ index.php
@@ -1,1 +1,27 @@
-<?php echo "PHP 작동!"; ?>
+<?php include 'includes/header.php'; ?>
+
+<div class="d-flex">
+    <aside class="sidebar">
+        <nav>
+            <a href="#">GitHub.</a>
+            <a href="#">LinkedIn.</a>
+            <a href="#">Tistory Blog.</a>
+            <a href="#">Velog.</a>
+            <a href="#">YouTube.</a>
+        </nav>
+        <div class="contact">
+            <p>Oneriever@email.com</p>
+        </div>
+    </aside>
+
+    <!-- 메인 영역 -->
+    <main class="main-content">
+        <div class="hero">
+            <h2>스스로 빛내려는 원석인 개발자</h2>
+            <h1 class="highlight">김일강</h1>
+            <p>프론트 & 앱</p>
+        </div>
+    </main>
+</div>
+
+<?php include 'includes/footer.php'; ?>
